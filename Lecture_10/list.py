2e88c64e9752ff12e489039fdd652239557c13f5
Rachit Garg <rachitgarg1208@gmail.com>
2024-09-30 14:55:14 +0530

    Python

---
+++ Lecture_10/list.py
@@ -0,0 +1,8 @@
+tea_varities = ["Black","Green","Oolong","White"]
+print(tea_varities)
+
+print(tea_varities[0]) #Slicing
+print(tea_varities[1:3]) #Slicing
+
+tea_varities[3]= "Herbal"
+print(tea_varities)
---
+++ Lecture_14/03.py
@@ -0,0 +1,13 @@
+marks= int(input("Enter your marks"))
+
+if(marks<=60):
+    print("F")
+elif(marks<=69):
+    print("D")
+elif(marks<=79):
+    print("C")
+elif(marks<=89):
+    print("B")
+elif(marks<=100):
+    print("A")
+
